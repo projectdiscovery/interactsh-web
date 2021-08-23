36d0fca61b63b75aa95c264c2e31d64e739a9851
Jesse Kelly <kellyjesse881@gmail.com>
2021-08-23 08:01:57 -0500

    chore(test): updated localStorage test

--- src/lib/localStorage/index.test.ts
+++ src/lib/localStorage/index.test.ts
@@ -3,22 +3,23 @@ import * as E from "fp-ts/Either";
 
 import * as _ from ".";
 
+const testData = 12;
+const decoder = t.number.asDecoder();
+
 describe("localStorage", () => {
   it("writeStoredData", () => {
     jest.spyOn(window.localStorage.__proto__, "setItem");
-    _.writeStoredData("key")({ a: 1 })();
+    _.writeStoredData("key")(testData)();
 
     // Tests that localStorage.setItem is called with the correct data.
     expect(localStorage.setItem).toHaveBeenCalledWith(
       "key",
-      JSON.stringify({ a: 1 })
+      JSON.stringify(testData)
     );
   });
 
   it("getStorageData", () => {
-    const decoder = t.interface({ a: t.number }).asDecoder();
-
-    _.writeStoredData("key")({ a: 1 })(); // Prep stored data.
+    _.writeStoredData("key")(testData)(); // Prep stored data.
     const data = _.getStoredData("key", decoder)();
 
     jest.spyOn(window.localStorage.__proto__, "getItem");
@@ -28,6 +29,6 @@ describe("localStorage", () => {
     expect(localStorage.getItem).toHaveBeenCalledWith("key");
 
     // Test that data is retreived and decoded correctly.
-    expect(data).toEqual(E.right({ a: 1 }));
+    expect(data).toEqual(E.right(testData));
   });
 });
